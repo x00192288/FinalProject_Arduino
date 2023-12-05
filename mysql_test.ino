bab6cb6cb4049fada3b118b21933664b893250a4
x00192288 <124812133+x00192288@users.noreply.github.com>
2023-12-05 21:16:05 +0000

    temperature from IMU sensor added, adds value to database in 60 second intervals in loop

--- mysql_test.ino
+++ mysql_test.ino
@@ -1,26 +1,16 @@
-/*
- * Created by ArduinoGetStarted.com
- *
- * This example code is in the public domain
- *
- * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-mysql
- */
-
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
+#include <Arduino_LSM6DS3.h>
+
 
 #include "arduino_secrets.h" 
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;                 // your network key index number (needed only for WEP)
+char ssid[] = SECRET_SSID;        //  network SSID (name)
+char pass[] = SECRET_PASS;    //  network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                 //  network key index number (needed only for WEP)
 
-int status = WL_IDLE_STATUS;
-//
-//IPAddress server_addr(192,168,1,5);  // IP of the MySQL *server* here
-//char user[] = "Arduino";              // MySQL user login username
-//char password[] = "x0192288!";   
+int status = WL_IDLE_STATUS; 
 
 WiFiServer server(80);
 
@@ -31,12 +21,12 @@ int    HTTP_PORT   = 80;
 String HTTP_METHOD = "GET";
 char   HOST_NAME[] = "172.20.10.3"; // change to your PC's IP address
 String PATH_NAME   = "/insert_temp.php";
-String queryString = "?temperature=43";
+//String queryString = "?temperature=44";
 
 void setup() {
   Serial.begin(9600);
 
-  // initialize the Ethernet shield using DHCP:
+  // initialize Wifi
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -64,16 +54,50 @@ void setup() {
     delay(10000);
   }
   server.begin();
-  // you're connected now, so print out the status:
+  // connected now, print out the status:
   printWifiStatus();
 
 
-  // you're connected now, so print out the data:
+  // connected now, print out the data:
   Serial.println("You're connected to the network");
 
-  if (client.connect(HOST_NAME, 80))
+
+if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+
+    while (1);
+  }
+
+  Serial.print("Temperature sensor sample rate = ");
+  Serial.print(IMU.temperatureSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Temperature reading in degrees C");
+  Serial.println("T");
+}
+
+void loop() {
+  
+unsigned long previousMillis = 0;
+const unsigned long interval = 600000; // 60 second interval
+
+while (1) {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+float t;
+
+  if (IMU.temperatureAvailable()) {
+    // after IMU.readTemperature() returns, t will contain the temperature reading
+    
+    IMU.readTemperature(t);
+
+
+    Serial.println(t);
+    if (client.connect(HOST_NAME, 80))
   {
-    // REPLACE WITH YOUR SERVER ADDRESS
+    String queryString = "?temperature=" + String(t);
+
     Serial.println("connected");
     client.println(HTTP_METHOD + " " + PATH_NAME + queryString + " HTTP/1.1");
     Serial.println("method correct");
@@ -108,44 +132,23 @@ void setup() {
   } else {// if not connected:
     Serial.println("connection failed");
   }
-  //printCurrentNet();
-  //printWifiData();
-
-//  // connect to web server on port 80:
-// if(client.connect(HOST_NAME, HTTP_PORT)) {
-//    // if connected:
-//    Serial.println("Connected to server");
-//    // make a HTTP request:
-//    // send HTTP header
-//    client.println(HTTP_METHOD + " " + PATH_NAME + queryString + " HTTP/1.1");
-//    client.println("Host: " + String(HOST_NAME));
-//    client.println("Connection: close");
-//    client.println(); // end HTTP header
-//
-//    while(client.connected()) {
-//      if(client.available()){
-//        // read an incoming byte from the server and print it to serial monitor:
-//        char c = client.read();
-//        Serial.print(c);
-//      }
-//    }
-//
-//    // the server's disconnected, stop the client:
-//    client.stop();
-//    Serial.println();
-//    Serial.println("disconnected");
-//  } else {// if not connected:
-//    Serial.println("connection failed");
-//  }
-}
-
-void loop() {
-//int temperature;
-//temperature = 1;
-//
-//client.print(temp);
+  }
+    // Update the previousMillis to the current time
+    previousMillis = currentMillis;
+  }
   
+
+
+
+  
+
+
+
+
+
+
 }
+}// LOOP
 
 
 void printWifiStatus() {
@@ -153,7 +156,7 @@ void printWifiStatus() {
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
+  // print board's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
